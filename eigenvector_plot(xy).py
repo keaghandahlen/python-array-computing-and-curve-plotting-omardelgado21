a1242b17e803eecc0b6132d27d4220de320dae70
keaghandahlen <124001556+keaghandahlen@users.noreply.github.com>
2023-03-07 10:22:47 -0600

    Create eigenvector_plot(xy).py

---
+++ eigenvector_plot(xy).py
@@ -0,0 +1,13 @@
+#eigenfuntion plot for n=5 and n=10
+# like in matlab
+
+from numpy import linspce
+
+xrange = linspace(0,1,10)
+
+yrange = linspace(0,1,1)
+
+sigma = linspce(0.5, 0.5, 1.5)
+m = 5
+
+legend()
